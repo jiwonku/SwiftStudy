26d74d6f9fcf7d6dce91df24b2443f80588ad4d2
wonkuji <wonkuji@naver.com>
2022-05-10 22:42:21 +0900

    swift study - 첫 번째

--- SwiftStudy/SwiftStudy/ViewController.swift
+++ SwiftStudy/SwiftStudy/ViewController.swift
@@ -12,6 +12,18 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        var isDarkMode : Bool = false
+        
+        if isDarkMode {
+            NSLog("this is dark mode")
+        }
+        else {
+            NSLog("this is not dark mode")
+        }
+        
+        var title : String = isDarkMode ? "this is dark mode" : "this is not dark mode"
+        NSLog("title is \(title)")
     }
 
 
