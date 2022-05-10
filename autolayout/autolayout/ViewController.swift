a55efb348936397853144f40f90b79837ca2cfd1
wonkuji <wonkuji@naver.com>
2022-05-10 22:33:50 +0900

    지난 내용 반영

---
+++ autolayout/autolayout/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  autolayout
+//
+//  Created by 지원구 on 2022/04/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ autolayout/autolayout/myTableViewCell.swift
@@ -0,0 +1,8 @@
+//
+//  myTableViewCell.swift
+//  autolayout
+//
+//  Created by 지원구 on 2022/04/18.
+//
+
+import Foundation
